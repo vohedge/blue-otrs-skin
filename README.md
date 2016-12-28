5368c3348031db2cf86dc910a5b9e8ad15057e98
Noah Kobayashi <vohedge@gmail.com>
2016-12-28 19:12:02 +0000

    file move

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 This is OTRS skin.
+
+## Installation
+
+```
+$ cd /opt/otrs/Custom
+$ git clone https://github.com/vohedge/blue-otrs-skin.git
+$ sudo ./install.sh
+``` 
+
---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+SKIN_PATH=`/bin/pwd`
+OTRS_USER="otrs"
+OTRS_GROUP="www-data"
+OTRS_ROOT="/opt/otrs"
+
+#make theme directory
+/bin/mkdir -p $OTRS_ROOT/Kernel/Output/HTML/BlueSkin
+
+#Fix permissions and make the proper symlinks
+/bin/chown -R $OTRS_USER:$OTRS_GROUP $SKIN_PATH
+/bin/chown -R $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/Kernel/Output/HTML/BlueSkin
+/usr/bin/find $SKIN_PATH/skins/ -type f -print0 | xargs -0 /bin/chmod 0660
+/usr/bin/find $SKIN_PATH/skins/ -type d -print0 | xargs -0 /bin/chmod 2770
+ 
+/bin/ln -s $SKIN_PATH/BlueSkin.xml $OTRS_ROOT/Kernel/Config/Files/ || true
+/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/Kernel/Config/Files/BlueSkin.xml
+/bin/ln -s $SKIN_PATH/skins/Agent/blueskin $OTRS_ROOT/var/httpd/htdocs/skins/Agent || true
+/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/var/httpd/htdocs/skins/Agent/blueskin
+# /bin/ln -s $SKIN_PATH/skins/Customer/blueskin $OTRS_ROOT/var/httpd/htdocs/skins/Customer || true
+# /bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/var/httpd/htdocs/skins/Customer/blueskin
+
+/bin/cp -r $SKIN_PATH/theme/BlueSkin $OTRS_ROOT/Kernel/Output/HTML
+
+echo ""
+echo "==========================================================="
+echo "All set. Now you can use the eionet skin and theme in otrs"
+echo "==========================================================="
+echo ""
+
